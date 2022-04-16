45a41107e7d1628c0480481cad8ea3d3cc2dd47b
[KanykeiKarataeva] <[kanysh150@gmail.com]>
2022-04-16 18:31:30 +0600

    First commit

---
+++ MyPanel.java
@@ -0,0 +1,66 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+
+class MyPanel extends JPanel{
+	public int index=0;
+	public void paint(Graphics g){
+		super.paint(g);
+		coordinates(g);
+		switch(index){
+			case 0: break;
+			case 1: function1(g); break;
+			case 2: function2(g); break;
+			case 3: function3(g); break;
+		}
+	}
+	public void coordinates(Graphics g){
+		Graphics2D graphic=(Graphics2D) g;
+		graphic.setStroke(new BasicStroke(1f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND, 10.0f, new float[] {16.0f,0f}, 0.0f));
+		graphic.setPaint(Color.black);
+		
+		graphic.drawRect(20,20,400,400);
+		graphic.drawLine(20, 220, 420, 220);
+		graphic.drawLine(220, 20, 220, 420);
+		
+		Font font=new Font("Tahoma",Font.BOLD, 14);
+		graphic.setFont(font);
+		graphic.drawString("x",410,215);
+		graphic.drawString("y",225,35);
+		graphic.drawString("0",225,235);
+	}
+	
+	public void function1(Graphics g){
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setStroke(new BasicStroke(2f));
+        g2.setPaint(Color.blue);
+        g2.drawLine(25, 415, 415, 25);
+	}
+	
+	public void function2(Graphics g){
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setStroke(new BasicStroke(2f));
+        g.setColor(Color.red);
+        Path2D func2 = new Path2D.Double();
+        func2.moveTo(120,25);
+        func2.quadTo(215,415, 330,25);
+        g2.draw(func2);
+	}
+	
+	public void function3(Graphics g){
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setStroke(new BasicStroke(2f));
+        g.setColor(Color.green);
+        Path2D func3 = new Path2D.Double();
+        func3.moveTo(225,25);
+        func3.quadTo(220,230, 415,215);
+		func3.moveTo(25,225);
+        func3.quadTo(220,230, 215,415);
+        g2.draw(func3);
+	}
+	
+	public void Change(int i){
+		index=i;
+		repaint();
+	}
+}
---
+++ MyWindow.java
@@ -0,0 +1,66 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class MyWindow extends JFrame {
+	private JButton button1;
+	private JButton button2;
+	private JButton button3;
+	private MyPanel panel;
+	private JLabel label;
+	Container contain; 
+	
+	MyWindow(){
+		super("Graphics window");
+		this.setBounds(20,20,800,600);
+		contain = this.getContentPane();
+		contain.setLayout(null);
+		label = new JLabel("Select function:");
+		label.setBounds(55, 55, 100, 20);
+		contain.add(label);
+
+		MyListener listener = new MyListener();
+		
+		button1 = new JButton("f(x) = x");
+		button1.addActionListener(listener);
+		button1.setBounds(55, 80, 120, 30);
+		contain.add(button1);
+
+		button2 = new JButton("f(x) = x^2");
+		button2.addActionListener(listener);
+		button2.setBounds(55, 120, 120, 30);
+		contain.add(button2);
+
+		button3 = new JButton("f(x) = 1/x");
+		button3.addActionListener(listener);
+		button3.setBounds(55, 160, 120, 30);
+		contain.add(button3);
+
+		panel = new MyPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(200, 50, 450, 450);
+		contain.add(panel);
+	}
+	class MyListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			if(e.getSource() == button1){
+				panel.Change(1);
+			}
+			else if(e.getSource() == button2){
+				panel.Change(2);
+			}
+			else if(e.getSource() == button3){
+				panel.Change(3);
+			}
+		}
+	}
+}
+
+class Example11{
+	public static void main(String[] args){
+		MyWindow window=new MyWindow();
+		window.repaint();
+		window.setVisible(true);
+	}
+}
